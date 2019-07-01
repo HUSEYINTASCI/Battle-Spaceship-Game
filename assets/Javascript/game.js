7537a82ab03373ad0a92d0c79726b9870f950f9c
huseyintasci <huseyintasci@yandex.com>
2019-07-01 16:31:19 -0700

    Created Bakround Sound

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,3 +1,11 @@
+function hide(id){
+    var divObject=document.getElementById(id);
+    divObject.style.display="none";
+}
+function show(id){
+    var divObject=document.getElementById(id);
+    divObject.style.display="block";
+}	
 
-	
-	
+
+
