1b71f9f6a2ccc220de2cbdc83ab14235b65e5a0a
huseyintasci <huseyintasci@yandex.com>
2019-07-04 22:01:02 -0700

    Intro added

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -10,19 +10,47 @@ function show(id){
     divObject.style.display="block";
 }	
 
+//intro timeout
+ 
+var y = document.querySelector("#intro");
+setTimeout(function(){ y.style.display = "none" }, 5000);
+var x = document.querySelector("#board");
+setTimeout(function(){ x.style.display = "none" }, 60000);
+
+
+
+   
 // User chose
 
- 
 
+var i = document.querySelector("img");
+
+function hideImage(){
+    if (event.key == "k"){
+        i.classList.add('hide');
+    }
+}
+
+document.onkeypress = hideImage;
+
+
+// document.onmouseup = function(event) {
+//     console.log('click')
+// }
+
+// i.addEventListener("mouseup", function(event) {
+//     i.classList.add('hide');
+//     document.mouseup=hideImage;
+//     })
 
 // Computer chose
 
-var colleges = ["0","1","2","3","4","5","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35"]
+var colleges = ["s0","s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","s12","s13","s14","s15","s16","s17","s18","s19","s20","s21","s22","s23","s24","s25","s26","s27","s28","s29","s30","s31","s32","s33","s34","s35"]
 
         console.log(Math.random());
         console.log(Math.random()*colleges.length);
  
-	    var ranIndex = Math.floor(Math.random()*colleges.length);
+	    var com = Math.floor(Math.random()*colleges.length);
         console.log(colleges[Math.floor(Math.random()*colleges.length)]);    
         
          
